9907f4bb8e018c40127d9a3e992ab2682681345f
IslamKadeev <108129869+IslamKadeev@users.noreply.github.com>
2022-07-14 15:51:30 +0300

    fixed decorator

--- homework3_task2.py
+++ homework3_task2.py
@@ -13,25 +13,25 @@ def repeater(call_count: int, start_sleep_time: int, factor: int, border_sleep_t
             print(f"Начало работы.")
 
             for i in range(1, call_count + 1):
+                test_t: int = start_sleep_time * (factor ** i)
+
+                if test_t < border_sleep_time and i != 1:
+                    t = test_t
+                elif test_t >= border_sleep_time:
+                    t = border_sleep_time
+
                 sleep(t)
                 func_result: int = function(*args)
 
                 print(f"Запуск номер {i}. Ожидание: {t} sec.", end=" ")
                 print(f"Результат декорируемой функции = {func_result}")
 
-                if t < border_sleep_time:
-                    t *= factor
-                else:
-                    t = border_sleep_time
-
             print("Конец работы.")
-
         return wrapper
-
     return repeater_decorator
 
 
-@repeater(5, 1, 2, 60)
+@repeater(5, 1, 2, 16)
 def func(number: int) -> int:
     return number + 1
 
